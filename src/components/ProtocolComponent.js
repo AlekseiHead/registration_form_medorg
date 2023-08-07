8fafcffb49dffd6b0d81f6cb5a483d28b4fa5035
Aleksei Golovanev <head.dev22@gmail.com>
2023-08-07 13:34:40 +0300

    init project with react app

---
+++ src/components/ProtocolComponent.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const ProtocolComponent = () => {
+	return (
+		<h1>Форма протокола
+		рентгенологического исследования</h1>
+	)
+}
+
+export default ProtocolComponent;
